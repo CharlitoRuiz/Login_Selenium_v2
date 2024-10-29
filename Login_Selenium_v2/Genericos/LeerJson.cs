78e8c936d2bb23eb4b313f354bb1dbcfa5efc14b
Carlos Ruiz <cruizfreak@hotmail.com>
2024-10-29 13:18:46 -0600

    Se agrega bloques try/catch para manejo de excepciones

--- Login_Selenium_v2/Genericos/LeerJson.cs
+++ Login_Selenium_v2/Genericos/LeerJson.cs
@@ -1,10 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace Login_Selenium_v2.Genericos
 {
@@ -12,24 +6,19 @@ namespace Login_Selenium_v2.Genericos
     {
         public List<POCO.LoginData> login_data()
         {
-            var Json = JsonConvert.DeserializeObject<Dictionary<String, List<POCO.LoginData>>>(File.ReadAllText(@"..\..\..\Data\login.json"));
-            return Json["credenciales"]; 
-
-            //try
-            //{
-            //    var jsonData = File.ReadAllText(@"..\..\..\Data\login.json");
-            //    var loginData = JsonConvert.DeserializeObject<POCO.LoginData>(jsonData);
-            //    return loginData ?? throw new InvalidOperationException("El archivo JSON está vacío o mal formateado.");
-            //}
-            //catch (FileNotFoundException)
-            //{
-            //    throw new FileNotFoundException("No se encontró el archivo JSON en la ruta especificada.");
-            //}
-            //catch (JsonException)
-            //{
-            //    throw new JsonException("El archivo JSON tiene un formato incorrecto.");
-            //}
-
+            try
+            {
+                var Json = JsonConvert.DeserializeObject<Dictionary<String, List<POCO.LoginData>>>(File.ReadAllText(@"..\..\..\Data\login.json"));
+                return Json["credenciales"] ?? throw new InvalidOperationException("El archivo JSON está vacío o mal formateado."); 
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException("No se encontró el archivo JSON en la ruta especificada.");
+            }
+            catch (JsonException)
+            {
+                throw new JsonException("El archivo JSON tiene un formato incorrecto.");
+            }
 
         }
     }
